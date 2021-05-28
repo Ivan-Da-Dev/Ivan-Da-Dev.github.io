77093d2a21a566372839ab9212a82f555744d114
Deus <67128188+Ivan-Da-Dev@users.noreply.github.com>
2021-05-28 15:34:31 +0800

    Add files via upload

--- pages/review/[review].js
+++ pages/review/[review].js
@@ -15,7 +15,7 @@ export default function Review({ reviewObj }){
       title: review.title
     }
 
-    const bgImg = `../review-images/${review.bg}`
+    const bgImg = `https://oniichann.tk/review-images/${review.bg}`
     const title = `${review.title.substring(0,1).toUpperCase()}${review.title.slice(1)}`
     const meta_desc = `Read Onii-chan's review on ${review.title} with a rating of ${review.rating}. ${review.desc}`
     const _review = 
