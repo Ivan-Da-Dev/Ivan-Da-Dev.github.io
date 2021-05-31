6b46b6dbedef69d3cc729ef891afea84a23b32c5
Deus <67128188+Ivan-Da-Dev@users.noreply.github.com>
2021-05-31 17:22:51 +0800

    Add files via upload

---
+++ components/navbar.js
@@ -0,0 +1,9 @@
+export default function () {
+    return (
+        <div className="nav_bar">
+        <a href="https://oniichann.tk">Home</a>
+        <a href="https://oniichann.tk/waifus">Waifus</a>
+        <a href="https://oniichann.tk/reviews">Reviews</a>
+        </div>
+    )
+}
